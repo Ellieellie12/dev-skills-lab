f74b91611fa4f8bb4e75b4c18f33a794576468ed
Ellie <eagyeman1234@gmail.com>
2023-09-14 10:50:15 -0400

    add data folder with fruits array

---
+++ data/fruit-data.js
@@ -0,0 +1,9 @@
+const fruits = [
+  {favorite: 'Apple', color:'red', eaten:true},
+  {favorite: 'Pineapple', color:'yellow', eaten:true},
+  {favorite: 'Mango', color:'orange', eaten:true}
+]
+
+export {
+  fruits
+}
